3c8551cdda57eafce8285ff8921ea2ebcfd5df97
cfgs <cfgs@172.17.1.30>
2022-10-26 13:27:28 +0200

    Actualización  26.Octubre.2022

---
+++ src/arrays/Arrays.java
@@ -0,0 +1,42 @@
+package arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		// DECLARACION
+		int [] [] matriz;
+		
+		//CONSTRUCCION
+		matriz = new int[5] [3];
+		
+		//*****************
+		
+		//Declaracion y construccion (en la misma linea)
+		
+		int [] myArray = new int [5];
+		
+		//*****************
+		
+		// REFERENCIA DE MEMORIA
+		// I-Integer; D-DOUBLE; B-BYTE, Z-BOOLEANOS
+		
+		int[] miTabla = new int [10];
+		System.out.println(miTabla);
+		
+		//*****************
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+
+}
---
+++ src/arrays/garray.java
@@ -0,0 +1,28 @@
+// Ejercicio para sacar la nota media
+
+package arrays;
+
+import java.util.Scanner;
+import 
+
+public class garray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
