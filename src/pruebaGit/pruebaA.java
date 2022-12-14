70e1bc9cb41f08e60e9d8030835b3726f5a5045d
Vadok33 <a.elenanavarro@gmail.com>
2022-12-14 12:58:46 +0100

    Pruebas de git

---
+++ src/pruebaGit/pruebaA.java
@@ -0,0 +1,57 @@
+package pruebaGit;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class pruebaA {
+
+	public static void main(String[] args) {
+		
+		System.out.print("\n*** Inicio de programa");
+		System.out.println(" Ordenar Array ***");
+		
+		// VARIABLES
+		
+		int menor = 0;
+		int numElementos;
+		int i;
+		
+		/// LEER NUMERO TOTAL DE ELEMENTOS
+		
+		Scanner entrada = new Scanner(System.in);
+		do {
+			System.out.print("\nIntroduce el número total de elementos:");
+			numElementos = entrada.nextInt();
+		}
+		while(numElementos <= 0);
+		
+		// DECLARAR Y CONSTRUIR ARRAY
+		
+		int[] miArray = new int[numElementos];
+		
+		// RECOGER NUMEROS POR CONSOLA
+		
+		for(i = 0; i<numElementos; i++) {
+			System.out.println("Introduce número(" +(i+1)+ "): ");
+			miArray[i]=entrada.nextInt();
+			
+		}
+		// ESCRIBIR ARRAY SIN ORDENAR
+		
+		System.out.print("\nArray sin ordenar: ");
+		for(int num:miArray) {
+			System.out.print("[" +num +"] ");
+		}
+	       
+		// ORDENAR ARRAY CON SORT
+		
+	    System.out.println("\nArray ordenado de menor a mayor:" );
+	     
+	    Arrays.sort(miArray);
+	    Arrays.toString(miArray);
+	    
+	    System.out.println(Arrays.toString(miArray));
+	       }
+		
+	}
+
