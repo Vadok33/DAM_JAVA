8406c01f89e02908d0c84f505240f18fb6b7c4fa
Vadok33 <a.elenanavarro@gmail.com>
2023-01-27 14:19:27 +0100

    Cambios en el cajero

--- src/banco/CuentaCorriente.java
+++ src/banco/CuentaCorriente.java
@@ -1,68 +1,68 @@
 package banco;
 
-public class CuentaCorriente {
-	String dni;
 
-	String nombre;
+
+public class CuentaCorriente {
 	
-	double saldo;
+	// Atributos
 	
-public CuentaCorriente () {
-		
+	String dni;		// del titular
+	String nombre;	// del titular
+	double saldo;	// efectivo disponible en mi cuenta
+	
+	//Metodos CONSTRUCTORES
+	
+public CuentaCorriente () {		
 }
 public CuentaCorriente(String dni, String nombre, double saldo) {
 
 this.dni = dni;
-
 this.nombre = nombre;
-
 this.saldo = saldo;
-
 }
+
+//Métodos GETTERS & SETTERS
 public String getnombre() {
-
     return nombre;
-
 }
 
 public double getsaldo() {
-
     return saldo;
-
 }
 
 public String getdni() {
-
 	return dni;
-
 }
 
 public void setnombre(String nombre) {
-
     this.nombre = nombre;
-
 }
 
 public void setsaldo(double saldo) {
-
     this.saldo = saldo;
-
 }
 
 public void setdni(String dni) {
-
 	this.dni = dni;
-
-}
-public void retirar() {
-	
 }
 
-public void ingresar() {
-	
+//Métodos de OPERACIONES
+
+public void salir (){
+	System.out.println("Hasta pronto!");
 }
 
-public void consultar() {
+public void retirar(CuentaCorriente miCuenta) {
+	Scanner sc=new Scanner (System.in);
+	System.out.println("Tu saldo es: "+ miCuenta.saldo);
+	System.out.println("¿Cuanto dinero desea sacar?");
+}
+
+public void ingresar(CuentaCorriente miCuenta) {
+	Scanner sc=new Scanner (System.in);
+}
+
+public void consultar(CuentaCorriente miCuenta) {
 	
 }
 
